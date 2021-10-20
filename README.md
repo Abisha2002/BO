4f9e88615d2b5710728e0234e5a3ddd153cf96c4
abisha <33662@ma-web.nl>
2021-10-20 17:17:48 +0200

    vraag 4 bijgemaakt

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 ### Mijn naam is abisha en hier ga ik wat vertellen over mijn opdracht
 
 #### Ik zit in het eerste jaar op het Mediacollege Amsterdam en doe de opleiding Software Developer. 
-#### We hebben in de klas een meneer in de klas gehad. Ik heb een opdracht gemaakt waar ik een quizz heb gemaakt waar in je in de positie van een vluchtelling zit en alleen als je de juiste keuzes  maakt kom je er levend uit. 
+#### We hebben in de klas een meneer in de klas gehad die is gevlucht vanuit syrie, hij vertelde aan ons hoe dat precies is gegaan. Ik heb een quizz heb gemaakt waar in je in de positie van een vluchtelling zit en alleen als je de juiste keuzes  maakt kom je er levend uit.
--- vluchtellingquizz.py
+++ vluchtellingquizz.py
@@ -6,6 +6,20 @@ time.sleep(3)
 print("Je bent een vluchtelling in syrie die de optie krijgt om te vluchten van de oorlog naar een veilig land het is aan jou of je dat ook  gaat doen")
 
 #vragen 
+
+def vraag4():
+    print("Via welke route wil je gaan vluchten?")
+
+    
+
+
+
+
+
+
+
+
+
 def vraag3(): 
     print("Vind jij het een goed dat je broer zijn bestevriend wilt mee nemen?")  
     print("a = Ja goed idee!")  
@@ -14,9 +28,10 @@ def vraag3():
     if antwoord3.lower == "a":
         print("Je gaat samen vluchten met je broer en zijn bestevriend")
         time.sleep(1)
-        print(vraag4())
+        print(vraag6())
     elif antwoord4.lower() == "b":
         print("De beste vriend mag niet mee je broer wilt ook niet mee, je gaat nu alleen vluchten")
+        print(vraag4()) 
 
 
 def vraag2(): 
@@ -30,27 +45,30 @@ def vraag2():
         time.sleep(2)
         print(vraag3())
     elif antwoord2.lower() == "b":
-        print("je beste vriendin wilt niet mee vluchten")
+        print("je beste vriendin wilt niet mee vluchten, je gaat nu alleen vluchten")
         print(vraag4())        
+    elif antwoord2.lower() == "c":
+        print("je kiest ervoor om samen met je moeder te gaan vluchten")
+        print(vraag5())
 
 
 def vraag1():
-    print("Wil je gaan vluchten?")
-    print("a = ja ik wil vluchten")
-    print("b = nee ")
-    print("c = ja maar ik wil iemand meennemen")
+    print("Wil je gaan vluchten.?")
+    print("a = ja ik wil vluchten.")
+    print("b = nee. ")
+    print("c = ja maar ik wil iemand meennemen.")
     antwoord1 = input()
     if antwoord1.lower() == "a": 
         print("Je hebt gekozen om te vluchten.\n") 
         time.sleep(1)
-        print(vraag2())
+        print(vraag4())
     elif antwoord1.lower() == "b": 
         print("Je kiest ervoor om niet te gaan vluchten hierdoor ga je dood.")
         time.sleep(1)
         print(vraag1())
-        print("Je bent dood gegaan, probeer het opnieuw")
+        print("Je bent dood gegaan, probeer het opnieuw.")
     elif antwoord1.lower() == "c":
-        print("Wie wil je meenemen")
+        print("Je hebt gekozen om met iemand te vluchten.")
         print(vraag2())
     else:
         print("Kies a, b, c")
